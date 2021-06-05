85f02d99143610e6d11c3df523694de62158d3a5
Sudhakar Sundaram <sudhakar.sundaram@aspiresys.com>
2021-06-05 15:21:56 +0530

    DB string removed

--- server/index.js
+++ server/index.js
@@ -16,10 +16,10 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://admin:uW6DcX4FibAhmmrc@cluster0.gl87y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 

42bf1e1132b5c298b25473f4cba77bbe2bbea751
Sudhakar S <ssudhakarme@gmail.com>
2021-06-15 10:41:11 +0530

    Removed disable host & img update

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,8 +4,8 @@ import { Link, useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import decode from 'jwt-decode';
 
-import memoriesLogo from '../../images/memoriesLogo.png';
-import memoriesText from '../../images/memoriesText.png';
+import memoriesLogo from '../../images/memoriesLogo.PNG';
+import memoriesText from '../../images/memoriesText.PNG';
 import * as actionType from '../../constants/actionTypes';
 import useStyles from './styles';
 
